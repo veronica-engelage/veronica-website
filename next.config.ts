f8e195def884ab25e98fb853a969f82870043902
Henning Engelage <veronicaengelage@Veronicas-MacBook-Air-2.local>
2026-02-15 17:55:00 -0500

    Force www to non-www redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.veronicachs.com",
+          },
+        ],
+        destination: "https://veronicachs.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
